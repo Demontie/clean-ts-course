9fe9397df5c3f4eac64c9a3a3350b833e208102a
Demontie costa <demontiecosta17@gmail.com>
2021-04-22 19:49:34 -0300

    refactor: move password comparation to the ValidationComposite

---
+++ src/presentation/helpers/validators/compare-fields-validation.ts
@@ -0,0 +1,14 @@
+import { InvalidParamError } from '../../erros'
+import { Validation } from './validation'
+export class CompareFieldsValidation implements Validation {
+  constructor (
+    private readonly fieldName: string,
+    private readonly fieldTocompareName: string
+  ) {}
+
+  validate (input: any): Error {
+    if (input[this.fieldName] !== input[this.fieldTocompareName]) {
+      return new InvalidParamError(this.fieldTocompareName)
+    }
+  }
+}
