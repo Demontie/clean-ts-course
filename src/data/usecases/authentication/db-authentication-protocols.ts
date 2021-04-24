375f3e463efd84e5c7ac127f6a41eaeec080b0b7
Demontie costa <demontiecosta17@gmail.com>
2021-04-24 10:36:58 -0300

    feat: ensure DbAuthentication calls LoadAccountByEmailRepository with correct email

---
+++ src/data/usecases/authentication/db-authentication-protocols.ts
@@ -0,0 +1,4 @@
+export * from '../../../domain/usecases/authentication'
+export * from '../../../domain/models/account'
+export * from '../../protocols/encrypter'
+export * from '../../protocols/load-account-by-email-repository'
