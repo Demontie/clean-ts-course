544036710215d2f29657cace8ea73b91292282c6
Demontie costa <demontiecosta17@gmail.com>
2021-05-01 18:55:34 -0300

    feat: ensure DbLoadAccountByToken calls LoadAccountByTokenRepository with correct value

--- src/data/usecases/load-account-by-token/db-load-account-by-token-protocols.ts
+++ src/data/usecases/load-account-by-token/db-load-account-by-token-protocols.ts
@@ -1,4 +1,4 @@
 export * from '../../../domain/models/account'
 export * from '../../protocols/cryptography/decrypter'
-export * from '../../protocols/db/account/load-account-by-email-repository'
+export * from '../../protocols/db/account/load-account-by-token-repository'
 export * from '../../../domain/usecases/load-account-by-token'
