ecbe42264d187a4f5fe2f4fef0db894b7082c93c
Demontie costa <demontiecosta17@gmail.com>
2021-05-01 10:38:09 -0300

    fix: describe test

--- src/presentation/middlewares/auth-middleware.spec.ts
+++ src/presentation/middlewares/auth-middleware.spec.ts
@@ -12,8 +12,8 @@ const makeSut = (): SutTypes => {
   }
 }
 
-describe('', () => {
-  test('', async () => {
+describe('Auth Middleware', () => {
+  test('Should return 403 if no x-access-token exists in headers', async () => {
     const { sut } = makeSut()
     const httpResponse = await sut.handle({})
     expect(httpResponse).toEqual(forbidden(new AccessDeniedError()))
