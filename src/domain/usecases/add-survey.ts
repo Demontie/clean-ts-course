8083d04db5f0ee02cc102ea041d28df9eb53a438
Demontie costa <demontiecosta17@gmail.com>
2021-05-03 22:22:29 -0300

    feat: ensure LoadSurveysController calls LoadSurveysUseCase

--- src/domain/usecases/add-survey.ts
+++ src/domain/usecases/add-survey.ts
@@ -1,13 +1,8 @@
-
+import { SurveyAnswersModel } from '../models/survey'
 export interface AddSurveyModel {
   question: string
-  answers: SurveyAnswers[]
+  answers: SurveyAnswersModel[]
   date: Date
-}
-
-export interface SurveyAnswers {
-  image?: string
-  answer: string
 }
 
 export interface AddSurvey {
