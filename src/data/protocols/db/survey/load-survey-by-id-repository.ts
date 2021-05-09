8f97adad033f04e2226199453ae485e872eac47e
Demontie costa <demontiecosta17@gmail.com>
2021-05-08 21:25:27 -0300

    feat: ensure DbSaveSurveyResult Usecase calls SaveSurveyResultRepository with correct values

--- src/data/protocols/db/survey/load-survey-by-id-repository.ts
+++ src/data/protocols/db/survey/load-survey-by-id-repository.ts
@@ -1,4 +1,4 @@
-import { SurveyModel } from '@/data/usecases/load-surveys/db-load-surveys-protocols'
+import { SurveyModel } from '@/domain/models/survey'
 
 export interface LoadSurveyByIdRepository {
   loadById: (id: string) => Promise<SurveyModel>
