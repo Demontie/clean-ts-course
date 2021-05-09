49a9f88e35879701152135076538ebc6e2f4c8e0
Demontie costa <demontiecosta17@gmail.com>
2021-05-08 21:48:19 -0300

    test: ensure SurveyMongoRepository load survey by id on success

--- src/infra/db/mongodb/survey/survey-mongo-repository.spec.ts
+++ src/infra/db/mongodb/survey/survey-mongo-repository.spec.ts
@@ -62,4 +62,13 @@ describe('Survey Mongo Repository', () => {
       expect(surveys.length).toBe(0)
     })
   })
+
+  describe('loadById()', () => {
+    test('Should load survey by id on success', async () => {
+      const res = await surveyColletions.insertOne(makeFakeAddSurveyModel())
+      const sut = makeSut()
+      const survey = await sut.loadById(res.ops[0]._id)
+      expect(survey).toBeTruthy()
+    })
+  })
 })
