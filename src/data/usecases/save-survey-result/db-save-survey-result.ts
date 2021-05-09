e1fb923303039232e79f972c0853f7340121645b
Demontie costa <demontiecosta17@gmail.com>
2021-05-08 21:32:28 -0300

    feat: ensure DbSaveSurveyResult return survey result on success

--- src/data/usecases/save-survey-result/db-save-survey-result.ts
+++ src/data/usecases/save-survey-result/db-save-survey-result.ts
@@ -11,7 +11,7 @@ export class DbSaveSurveyResult implements SaveSurveyResult {
   ) {}
 
   async save (data: SaveSurveyResultModel): Promise<SurveyResultModel> {
-    await this.saveSurveyResultRepository.save(data)
-    return null
+    const saveResultData = await this.saveSurveyResultRepository.save(data)
+    return saveResultData
   }
 }
