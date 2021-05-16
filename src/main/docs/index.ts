a23b65ac68321c5721ce6040279f38c6e4b13762
Demontie costa <demontiecosta17@gmail.com>
2021-05-16 16:25:45 -0300

    docs: update login api docs

--- src/main/docs/index.ts
+++ src/main/docs/index.ts
@@ -1,12 +1,17 @@
-import { loginPath } from './paths/login-path'
-import { accountShema } from './schemas/account-schema'
-import { loginParamsShema } from './schemas/login-params-schema'
+import { loginPath } from './paths'
+import { badRequest, serverError, unauthorized, notFound } from './components'
+import { accountShema, loginParamsShema, errorShema } from './schemas'
+
 export default {
   openapi: '3.0.0',
   info: {
     title: 'Clean node API',
     description: 'API do curso do Mango para realizar enquetes',
     version: '1.0.0'
+  },
+  license: {
+    name: 'GPL-3.0-or-later',
+    url: 'https://spdx.org/licenses/GPL-3.0-or-later.html'
   },
   servers: [{
     url: '/api'
@@ -19,6 +24,13 @@ export default {
   },
   schemas: {
     account: accountShema,
-    loginParams: loginParamsShema
+    loginParams: loginParamsShema,
+    error: errorShema
+  },
+  components: {
+    badRequest,
+    serverError,
+    unauthorized,
+    notFound
   }
 }
