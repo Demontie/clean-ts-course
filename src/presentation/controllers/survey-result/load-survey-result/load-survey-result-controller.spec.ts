9ce955c829f8baabb55af51c5019a90c006dc5d1
Demontie costa <demontiecosta17@gmail.com>
2021-05-29 16:50:53 -0300

    test: ensure LoadSurveyResultController calls LoadSurveyById with correct values

---
+++ src/presentation/controllers/survey-result/load-survey-result/load-survey-result-controller.spec.ts
@@ -0,0 +1,44 @@
+import { LoadSurveyResultController } from './load-survey-result-controller'
+import { HttpRequest, LoadSurveyResult, LoadSurveyById } from './load-survey-result-controller-protocols'
+import { mockLoadSurveyById, mockLoadSurveyResult } from '@/presentation/test'
+import MockDate from 'mockdate'
+
+const mockRequest = (): HttpRequest => ({
+  params: {
+    surveyId: 'any_id'
+  }
+})
+
+type SutTypes = {
+  sut: LoadSurveyResultController
+  loadSurveyByIdStub: LoadSurveyById
+  loadSurveyResultStub: LoadSurveyResult
+}
+
+const makeSut = (): SutTypes => {
+  const loadSurveyByIdStub = mockLoadSurveyById()
+  const loadSurveyResultStub = mockLoadSurveyResult()
+  const sut = new LoadSurveyResultController(loadSurveyByIdStub, loadSurveyResultStub)
+  return {
+    sut,
+    loadSurveyByIdStub,
+    loadSurveyResultStub
+  }
+}
+
+describe('LoadSurveyResult Controller', () => {
+  beforeAll(() => {
+    MockDate.set(new Date())
+  })
+
+  afterAll(() => {
+    MockDate.reset()
+  })
+
+  test('Should call LoadSurveyById with correct values', async () => {
+    const { sut, loadSurveyByIdStub } = makeSut()
+    const loadByIdSpy = jest.spyOn(loadSurveyByIdStub, 'loadById')
+    await sut.handle(mockRequest())
+    expect(loadByIdSpy).toHaveBeenCalledWith('any_id')
+  })
+})
